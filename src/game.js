2dca42a1bcb4e06b7a3c0ea9d267ec7b5baf528c
dev-chenxing <chenxing.io@outlook.com>
2024-11-13 17:51:55 +0800

    normalized orientation vector

--- src/game.js
+++ src/game.js
@@ -5,20 +5,27 @@ export async function init() {
   k.setBackground([128, 180, 255]);
   k.loadSprite("bag", `./sprites/bag.png`);
 
-  const player = k.add(["player", k.sprite("bag"), k.pos(k.center()), k.anchor("center"), k.area({ shape: new k.Rect(k.vec2(4, 2), 48, 42) }), k.body(), { orientation: k.vec2(0, 0), speed: 320 }]);
+  const player = k.add(["player", k.sprite("bag"), k.pos(k.center()), k.anchor("center"), k.area({ shape: new k.Rect(k.vec2(4, 2), 48, 42) }), k.body(), { orientation: k.vec2(0, 0), speed: 160 }]);
 
   player.onUpdate(() => {
     // Set the viewport center to player.pos
-    k.camPos(player.worldPos());
+    // k.camPos(player.worldPos());
+
+    player.orientation = player.orientation.unit();
+    player.move(player.orientation.scale(player.speed));
   });
   k.onKeyDown("left", () => {
-    player.move(-player.speed, 0);
+    player.orientation.x = -1;
     player.flipX = true;
   });
   k.onKeyDown("right", () => {
-    player.move(player.speed, 0);
+    player.orientation.x = 1;
     player.flipX = false;
   });
-  k.onKeyDown("up", () => player.move(0, -player.speed));
-  k.onKeyDown("down", () => player.move(0, player.speed));
+  k.onKeyDown("up", () => (player.orientation.y = -1));
+  k.onKeyDown("down", () => (player.orientation.y = 1));
+  k.onKeyRelease("left", () => (player.orientation.x = 0));
+  k.onKeyRelease("right", () => (player.orientation.x = 0));
+  k.onKeyRelease("up", () => (player.orientation.y = 0));
+  k.onKeyRelease("down", () => (player.orientation.y = 0));
 }
